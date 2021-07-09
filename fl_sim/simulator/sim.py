b688be39d35030d07b8e4190681efdb6db16450f
NicholasRasi <nicholas.rasi@gmail.com>
2021-07-09 16:26:58 +0200

    added simple stopping conditions (accuracy, loss)

--- fl_sim/simulator/sim.py
+++ fl_sim/simulator/sim.py
@@ -42,11 +42,25 @@ class Simulator:
 
             self.logger.info("starting training...")
             start_ts = time.time()
-            for r in trange(self.config.simulation["num_rounds"]):
+            for r in range(self.config.simulation["num_rounds"]):
+                self.logger.info("* running round %d *" % (r+1))
+
                 self.fed_alg.model_fit(r)
                 self.fed_alg.model_eval(r)
+
+                self.logger.info("eval accuracy: %.4f | loss: %.4f" %
+                                 (status.var["eval"]["model_metrics"]["agg_accuracy"][r],
+                                  status.var["eval"]["model_metrics"]["agg_loss"][r]))
+
+                # check if the stopping conditions are met
+                if status.var["eval"]["model_metrics"]["agg_accuracy"][r] >= self.config.simulation["stop_conds"]["accuracy"]:
+                    self.logger.info("stopping condition (accuracy) met. %.4f acc reached" % status.var["eval"]["model_metrics"]["agg_accuracy"][r])
+                    break
+                if status.var["eval"]["model_metrics"]["agg_loss"][r] <= self.config.simulation["stop_conds"]["loss"]:
+                    self.logger.info("stopping condition (loss) met. %.4f loss reached" % status.var["eval"]["model_metrics"]["agg_loss"][r])
+                    break
             duration = time.time() - start_ts
-            self.logger.info(f"training completed in {duration} seconds")
+            self.logger.info(f"training completed in {duration:.2f} seconds")
 
             self.logger.info("saving run data")
             self.save_run_data(status)
