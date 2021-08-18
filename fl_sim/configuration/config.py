5848f8433958c1cfc87bb53a75fbacf2130b678f
NicholasRasi <nicholas.rasi@gmail.com>
2021-08-18 15:14:46 +0200

    Added import in configuration file
    A config file can be defined importing another config file

--- fl_sim/configuration/config.py
+++ fl_sim/configuration/config.py
@@ -4,14 +4,54 @@ import yaml
 class Config:
 
     def __init__(self, config_file):
-        # read config file
-        with open(config_file, 'r') as f:
-            config = yaml.load(f)
+        self.simulation = None
+        self.algorithms = None
+        self.devices = None
+        self.computation = None
+        self.energy = None
+        self.network = None
+        self.data = None
 
-        self.simulation = config["simulation"]
-        self.algorithms = config["algorithms"]
-        self.devices = config["devices"]
-        self.computation = config["computation"]
-        self.energy = config["energy"]
-        self.network = config["network"]
-        self.data = config["data"]
+        self.config = self.read_config(config_file)
+
+        self.simulation = self.config["simulation"]
+        self.algorithms = self.config["algorithms"]
+        self.devices = self.config["devices"]
+        self.computation = self.config["computation"]
+        self.energy = self.config["energy"]
+        self.network = self.config["network"]
+        self.data = self.config["data"]
+
+    def read_config(self, config_file):
+        config = Config.read_file(config_file)
+
+        if "import" in config:
+            import_file = config.pop("import")
+            return Config._merge_dictionaries(self.read_config(import_file), config)
+        else:
+            return config
+
+    @staticmethod
+    def read_file(config_file):
+        with open(config_file, 'r') as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+            return config
+
+    @staticmethod
+    def _merge_dictionaries(dict1, dict2):
+        """
+        Recursive merge dictionaries.
+
+        :param dict1: Base dictionary to merge.
+        :param dict2: Dictionary to merge on top of base dictionary.
+        :return: Merged dictionary
+        """
+        for key, val in dict1.items():
+            if isinstance(val, dict):
+                dict2_node = dict2.setdefault(key, {})
+                Config._merge_dictionaries(val, dict2_node)
+            else:
+                if key not in dict2:
+                    dict2[key] = val
+
+        return dict2
